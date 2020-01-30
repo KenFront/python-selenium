3384ee4f546dd4297fd306488f00fc185baecf3a
ken.wang <ken.wang@pentium.network>
2020-01-30 16:28:53 +0800

    fix: wait for search input rendering

--- case/shopee_tw/search_goods.py
+++ case/shopee_tw/search_goods.py
@@ -12,8 +12,10 @@ def search_goods(browser: WebDriver):
         search_word = "筆電"
         browser.get(page_index)
 
-        search_bar = browser.find_element_by_css_selector(
-            '.shopee-searchbar .shopee-searchbar-input__input')
+        search_bar = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, ".shopee-searchbar .shopee-searchbar-input__input"))
+        )
         search_bar.send_keys(search_word)
         search_bar.send_keys(Keys.ENTER)
         WebDriverWait(browser, 10).until(
